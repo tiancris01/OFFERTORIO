f634ed7150ba62c0b86ea05e64a8b07cd05c3b1b
Daniel Osorio <cristian.osorio@doublevpartners.com>
2024-03-07 19:48:04 -0500

    Refactor: comming back to the proyect

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:offertorio/app_core/config/firebase/firabese_config.dart';
 import 'package:offertorio/app_widget.dart';
+import 'package:offertorio/firebase_options.dart';
 
 Future<void> main() async {
-  await firabaseConfig();
-  runApp(
-    const ProviderScope(
-      child: AppWidget(),
-    ),
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+  runApp(const ProviderScope(child: AppWidget()));
 }
